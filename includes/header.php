de453f30947243db9421e9d43cb01c6dae758e80
Varun <varun@varun-HP-Pavilion-Notebook>
2018-02-24 04:18:13 +0530

    Fourth commit

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Watchin'?</title>
+	<link href="https://fonts.googleapis.com/css?family=Acme|Shadows+Into+Light|Source+Sans+Pro" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/styles.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="wrapper2">
+			<div class="container">
+				<div class="navbar">
+					<div class="brand">
+						<a href="index.php">
+							Watchin'?				
+						</a>
+					</div>
+				</div>
